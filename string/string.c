8669a90d9ab2e8a6984b6badb354d123829ffb92
Hamir Mahal <hamirmahal@gmail.com>
2022-02-21 15:49:33 -0800

    add compilation command to .c file for convenience

--- string/string.c
+++ string/string.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// cc --std=c11 -o string string.c -Wall
 int main (int argc, char* argv[]) {
     char* addressToMyString = "Hello, world!";
     fwrite(addressToMyString, sizeof(char), 500, stdout);
