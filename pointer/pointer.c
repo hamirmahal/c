db918ffbe11d6927972772d795513f6ba775ea28
Hamir Mahal <hamirmahal@gmail.com>
2022-04-20 12:25:08 -0700

    add code for practicing pointers

---
+++ pointer/pointer.c
@@ -0,0 +1,24 @@
+// printf
+#include <stdio.h>
+
+// malloc
+#include <stdlib.h>
+
+/*
+cc -std=gnu11 -o ./pointer pointer.c && ./pointer > pointer.c.output.txt
+*/
+
+int main() {
+  int *ptr = malloc(sizeof(int));
+  printf("*ptr %d\n\n", *ptr);
+
+  printf("ptr %p\n", ptr);
+  printf("&ptr %p\n", &ptr);
+  printf("&*ptr %p\n\n", &*ptr);
+
+  printf("before increment %d\n", *ptr);
+  *ptr = *ptr + 1;
+  printf("after increment %d\n", *ptr);
+
+  return 0;
+}
