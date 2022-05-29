2fc5d33ac67480517fdf2510b9662aea34c08c28
Hamir Mahal <hamirmahal@gmail.com>
2022-05-29 11:41:03 -0700

    add code that shows addresses after `fork`!

---
+++ fork/fork.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// for fork!
+#include <unistd.h>
+
+/*
+cc -Wall -std=gnu11 -o fork fork.c && ./fork
+*/
+
+int main(int argc, char *argv[]) {
+  fork();
+  int x = 3;
+  printf("location of code: %p\n", main);
+  printf("location of heap: %p\n", malloc(1));
+  printf("location of stack: %p\n", &x);
+  return x;
+}
