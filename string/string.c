e9430adfc39799a524c1272975c6b427dad86322
Hamir Mahal <hamirmahal@gmail.com>
2022-06-16 12:52:16 -0700

    rearrange compilation command comment for copying

--- string/string.c
+++ string/string.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-// cc --std=c11 -o string string.c -Wall
+/*
+cc -Wall --std=c11 -o string string.c
+*/
 int main (int argc, char* argv[]) {
     char* addressToMyString = "Hello, world!";
     fwrite(addressToMyString, sizeof(char), 500, stdout);
