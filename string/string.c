737680e29c4df33cfdf64bf54f75f82917cd74ea
Hamir Mahal <hamirmahal@gmail.com>
2022-07-11 09:20:53 -0700

    style: add newline to output for readability

--- string/string.c
+++ string/string.c
@@ -6,4 +6,5 @@ cc -Wall --std=c11 -o string string.c && ./string | tee stringOutput.txt
 int main (int argc, char* argv[]) {
     char* addressToMyString = "Hello, world!";
     fwrite(addressToMyString, sizeof(char), 500, stdout);
+    fwrite("\n", sizeof(char), 1, stdout);
 }
